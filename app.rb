d822740e1f6597427152a5aa81f934bd605cffe2
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-27 14:48:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,9 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
-
+  
+  get "/multiply/:num1/:num2" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
 end
